97b7dc9cba31a1f8f807fed23fabbc350442da36
katungi <dankatdennis@gmail.com>
2020-03-30 20:21:51 +0300

    added styling to all webpages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get '/o/:slug', to: 'links#show', as: :short
+  get '/i/:slug', to: 'links#show', as: :short
 
   #get 'new', to: 'links#new'
 
